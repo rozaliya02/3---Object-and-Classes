df8e53451ba85b85761ad2b1d4943db0d99594a9
Rozalia Ravnachka <103183688+rozaliya02@users.noreply.github.com>
2022-05-01 18:16:10 +0300

    Delete .history directory

--- .history/CityRecord_20220501151459.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-  let data = input.map(row => row.split(" | ").filter(x => x != ""));
-  console.log(data);
-}
-solve([
-  "| Town | Latitude | Longitude |",
-  "| Sofia | 42.696552 | 23.32601 |",
-  "| Beijing | 39.913818 | 116.363625 |",
-]);
